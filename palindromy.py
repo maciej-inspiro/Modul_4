bc3a45bbc638e0856e83fce5029efd564846517f
maciej-inspiro <68592516+maciej-inspiro@users.noreply.github.com>
2020-07-29 18:35:29 +0200

    poprawa umieszczenia dokumentacji funkcji

--- palindromy.py
+++ palindromy.py
@@ -1,13 +1,14 @@
 def palindrom(wyraz):
-    if(wyraz == wyraz[:: - 1]): #string[:: – 1] zwraca stringa w odwrotnej kolejności
-        print("To jest palindrom")
-    else:
-        print("To NIE jest palindrom")
-"""
+    """
         Funkcja palindrom, sprawdza czy dany wyraz (string) jest palindromem, tj. słowem, które czytane od lewej do prawej i od prawej do lewej brzmi tak samo (np. kajak)
         Argumenty funkcji:
         wyraz
         W argumentach funkcji naley wpisać wyraz, który funkcja ma sprawdzić pod kątem bycia palindromem np. palindrom("kajak")
-"""
+    """
+    if(wyraz == wyraz[:: - 1]): #string[:: – 1] zwraca stringa w odwrotnej kolejności
+        print("To jest palindrom")
+    else:
+        print("To NIE jest palindrom")
+
 
 palindrom("potop")
