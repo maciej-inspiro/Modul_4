896ed8eef8755e3218aa22b640ff3f7231c21564
maciej-inspiro <68592516+maciej-inspiro@users.noreply.github.com>
2020-08-18 13:09:18 +0200

    uproszczenie funkcji
    Funkcja zwraca True/False zamiast wypisywać komunikat.

--- palindromy.py
+++ palindromy.py
@@ -5,17 +5,8 @@ def palindrom(wyraz):
         wyraz
         W argumentach funkcji naley wpisać wyraz, który funkcja ma sprawdzić pod kątem bycia palindromem np. palindrom("kajak")
     """
-    if(wyraz == wyraz[:: - 1]): #string[:: – 1] zwraca stringa w odwrotnej kolejności
-        print("To jest palindrom")
-    else:
-        print("To NIE jest palindrom")
-
+    print(wyraz == wyraz[:: - 1]) #string[:: – 1] zwraca stringa w odwrotnej kolejności
 
 palindrom("potop")
-
-
-#lub można zrobić to tak (najprościej):
-def palindrom(wyraz):
-    print(wyraz == wyraz[:: - 1])
-
-palindrom("potop")
+palindrom("kajak")
+palindrom("dom")
