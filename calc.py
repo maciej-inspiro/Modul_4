6c706db302555d211f30544a7084c0a13028c7b8
maciej-inspiro <68592516+maciej-inspiro@users.noreply.github.com>
2020-08-18 13:04:03 +0200

    dodanie warunku sprawdzającego
    dodanie warunku sprawdzającego czy podane przez użytkownika wartości są liczbami rzeczywistymi

--- calc.py
+++ calc.py
@@ -2,25 +2,28 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 while True:
-    operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-    a = input("Podaj składnik 1. ")
-    b = input("Podaj składnik 2. ")
+    operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    a = float(input("Podaj składnik 1. "))
+    b = float(input("Podaj składnik 2. "))
+    typ = type(a) and type (b) is float
+    if typ is True:
+      print("OK, podane wartości są liczbami.")
     if operation == "1":
       wynik = float(a)+float(b) #float - liczba rzeczywista, int - liczba całkowita (ang. integer)
-      logging.info("Dodaję " + a + " i " + b)
+      logging.info(f"Dodaję {a}  i {b}")
       print(f'Wynik to:  {wynik}')
     elif operation == "2":
       wynik = float(a)-float(b)
-      logging.info("Odejmuję " + a + " i " + b)
+      logging.info(f"Odejmuję {a}  i {b}")
       print(f'Wynik to:  {wynik}')
     elif operation == "3":
       wynik = float(a)*float(b)
-      logging.info("Mnożę " + a + " i " + b)
+      logging.info(f"Mnozę {a}  i {b}")
       print(f'Wynik to:  {wynik}')
     elif operation == "4":
       wynik = float(a)/float(b)
-      logging.info("Dzielę " + a + " i " + b)
+      logging.info(f"Dzielę {a}  i {b}")
       print(f'Wynik to:  {wynik}')
     else:
       print("Nie wybrałeś właściciwej opcji. Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-    
+
