02c06637275dea8fdc0e4a21376d1b902a3c7c57
maciej-inspiro <68592516+maciej-inspiro@users.noreply.github.com>
2020-09-03 08:57:11 +0200

    dodanie funkcji valid
    Funkcja valid sprawdza, czy wartość, którą podał użytkownik jest liczbą.

--- calc.py
+++ calc.py
@@ -1,16 +1,19 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-while True:
+def valid(s):
+    if not s.replace('.', '').replace('+', '').replace('-', '').isdigit():
+        return False
+    if s.count('.') > 1:
+        return False
+    return True
+
+while True: #pętla nieskończona
     operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
     a = input("Podaj składnik 1. ")
-    if a.isdigit():
-        logging.debug("OK")
-    else: logging.debug("Podana wartość nie jest liczbą!")
+    print(valid(a)) #weryfikacja czy to co podał uzytkownik jest liczbą
     b = input("Podaj składnik 2. ")
-    if b.isdigit():
-        logging.debug("OK")
-    else: logging.debug("Podana wartość nie jest liczbą!")
+    print(valid(b))
     if operation == "1":
       wynik = float(a)+float(b) #float - liczba rzeczywista, int - liczba całkowita (ang. integer)
       logging.info(f"Dodaję {a}  i {b}")
